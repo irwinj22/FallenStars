a673881a0162e8e5bcbe2fcc00d9323bf08dde82
gabrielriedel <gaberiedel@gmail.com>
2024-05-28 21:36:11 -0700

    major rec improvements

--- src/api/recommendations.py
+++ src/api/recommendations.py
@@ -13,40 +13,23 @@ router = APIRouter(
 )
 
 def cosine_distance(v1, v2):
+    v1 = np.array(v1) * np.array([0.001, 1, 1])
+    v2 = np.array(v2) * np.array([0.001, 1, 1])
+    print("v1", v1)
+    print("v2", v2)
     dot_product = np.dot(v1, v2)
     norm_v1 = np.linalg.norm(v1)
     norm_v2 = np.linalg.norm(v2)
     return 1-(dot_product / (norm_v1 * norm_v2))
 
 @router.post("/recommend")
-def recommend(budget: int, enemy_type: str, role: str):
+def recommend(budget: int, enemy_element: str, role: str):
 
-
-    # Offensively speaking, water beats Fire
-    # Earth beats Water
-    # Fire beats Earth
-    # 1 is associated with fire type objects, so if our enemy is of type earth we will want an attack object of fire type (1)
-    # Similarly 2 is for water objects, 3 is for earth, and 4 is basic
-    enemy_type_attack_map = {
-    "FIRE": 2,
-    "WATER": 3,
-    "EARTH": 1,
-    "BASIC": 4
-                    }
+     # The roles in our world fall into three categories: Attackers, Defenders, and Specialists
+    # Attackers prioritize weapons, which will be represented by 1
+    # Defenders prioritize armor, whcih will be represented by 2
+    # Specialists prioritize Misc. items, which will be represented by 3
     
-    # Defensively speaking, each type defends the best against its own type
-    # 1 for Fire, 2 for Water, 3 for Earth, 4 for Basic
-    enemy_type_defense_map = {
-    "FIRE": 1,
-    "WATER": 2,
-    "EARTH": 3,
-    "BASIC": 4
-                    }
-
-    # The roles in our world fall into three categories: Attackers, Defenders, and Specialists
-    # Attackers want weapons, which will be represented by 1
-    # Defenders want armor, whcih will be represented by 2
-    # Specialists want Misc. items, which will be represented by 3
     role_map = {
     "WARRIOR": 1,
     "SUNBLADE": 1,
@@ -59,43 +42,105 @@ def recommend(budget: int, enemy_type: str, role: str):
     "SCOUT": 3
                     }
     
-    object_type = role_map.get(role.upper(),0)
+    object_type = role_map.get(role.upper(),3)
 
-    # Attackers get weapons that are strong against enemy type
     if object_type == 1:
-        e_type = enemy_type_attack_map.get(enemy_type.upper(), 0)
-    
-    # Defender get armor of the same type as their enemy
+        w_budget = int(budget*0.4)
+        a_budget = int(budget*0.3)
+        m_budget = int(budget*0.3)
     elif object_type == 2:
-        e_type = enemy_type_defense_map.get(enemy_type.upper(), 0)
-    
-    # Misc. items are equally strong against all types, so specialists get a random type of misc. item
+        w_budget = int(budget*0.3)
+        a_budget = int(budget*0.4)
+        m_budget = int(budget*0.3)
     else:
-        e_type = random.randint(1, 4)
+        w_budget = int(budget*0.3)
+        a_budget = int(budget*0.3)
+        m_budget = int(budget*0.4)
+
+    # Offensively speaking, water beats Fire
+    # Earth beats Water
+    # Fire beats Earth
+    # 1 is associated with fire type objects, so if our enemy is of type earth we will want an attack object of fire type (1)
+    # Similarly 2 is for water objects, 3 is for earth, and 4 is basic
+    enemy_element_attack_map = {
+    "FIRE": 2,
+    "WATER": 3,
+    "EARTH": 1,
+    "BASIC": 4
+                    }
+    
+    
+    # Defensively speaking, each type defends the best against its own type
+    # 1 for Fire, 2 for Water, 3 for Earth, 4 for Basic
+    enemy_element_defense_map = {
+    "FIRE": 1,
+    "WATER": 2,
+    "EARTH": 3,
+    "BASIC": 4
+                    }
+    
+    w_element = enemy_element_attack_map.get(enemy_element.upper(), 4)
+    a_element = enemy_element_defense_map.get(enemy_element.upper(), 4)
+    m_element = random.randint(1, 4)
+
+    w_given_vec = [w_budget, w_element, 1]
+    a_given_vec = [a_budget, a_element, 2]
+    m_given_vec = [m_budget, m_element, 3]
     
 
 
     with db.engine.begin() as connection:
-        # Create our vector of given data
-        given_vec = [budget, e_type, object_type]
-        print(given_vec)
-        # Stores a list of all item vectors that we have in our item plan where we have at least one of those items in stock
-        item_vecs = connection.execute(sqlalchemy.text("""SELECT item_vec FROM items_plan
+        # Stores a list of all weapon item vectors that we have in our item plan where we have at least one of those items in stock
+        w_item_vecs = connection.execute(sqlalchemy.text("""SELECT item_vec FROM items_plan
                                                             JOIN items_ledger ON items_plan.id = items_ledger.item_id
+                                                            WHERE items_plan.type = :x
                                                             GROUP BY item_vec
-                                                            HAVING SUM(qty_change) > 0"""))
+                                                            HAVING SUM(qty_change) > 0"""), [{"x":"weapon"}])
         # Set cosine distance is worst possible
         min_dist = 2
+        # For each vector in our weapon inventory we will find the vector with the closest distance to the vector constructed with the user's info
+        for row in w_item_vecs:
+            if cosine_distance(row.item_vec, w_given_vec) < min_dist:
+                min_dist = cosine_distance(row.item_vec, w_given_vec)
+                print("MIN", min_dist)
+                w_rec_vec = row.item_vec
+        rec_weapon_sku = connection.execute(sqlalchemy.text("""SELECT sku FROM items_plan 
+                                                      WHERE item_vec = :x"""), [{"x":w_rec_vec}]).fetchone()
+        
+        # We repeat that process but for just the armor
+        a_item_vecs = connection.execute(sqlalchemy.text("""SELECT item_vec FROM items_plan
+                                                            JOIN items_ledger ON items_plan.id = items_ledger.item_id
+                                                            WHERE items_plan.type = :x
+                                                            GROUP BY item_vec
+                                                            HAVING SUM(qty_change) > 0"""), [{"x":"armor"}])
+        min_dist = 2
+        for row in a_item_vecs:
+            if cosine_distance(row.item_vec, a_given_vec) < min_dist:
+                min_dist = cosine_distance(row.item_vec, a_given_vec)
+                a_rec_vec = row.item_vec
+        rec_armor_sku = connection.execute(sqlalchemy.text("""SELECT sku FROM items_plan 
+                                                      WHERE item_vec = :x"""), [{"x":a_rec_vec}]).fetchone()
+        
+        # Lastly we repeat that process but for misc. items
+        m_item_vecs = connection.execute(sqlalchemy.text("""SELECT item_vec FROM items_plan
+                                                            JOIN items_ledger ON items_plan.id = items_ledger.item_id
+                                                            WHERE items_plan.type = :x
+                                                            GROUP BY item_vec
+                                                            HAVING SUM(qty_change) > 0"""), [{"x":"misc"}])
+        min_dist = 2
+        for row in m_item_vecs:
+            if cosine_distance(row.item_vec, m_given_vec) < min_dist:
+                min_dist = cosine_distance(row.item_vec, m_given_vec)
+                m_rec_vec = row.item_vec
+        rec_misc_sku = connection.execute(sqlalchemy.text("""SELECT sku FROM items_plan 
+                                                      WHERE item_vec = :x"""), [{"x":m_rec_vec}]).fetchone()
 
-        # For each vector in our inventory we will find the vector with the closest distance to the vector given by the user
-        for row in item_vecs:
-            if cosine_distance(row.item_vec, given_vec) < min_dist:
-                min_dist = cosine_distance(row.item_vec, given_vec)
-                rec_vec = row.item_vec
-        rec_item_sku = connection.execute(sqlalchemy.text("""SELECT sku FROM items_plan 
-                                                      WHERE item_vec = :x"""), [{"x":rec_vec}]).fetchone()
+        
     
-    # Return the sku of the item with the "closest" vector as our recommendation
+    # Return a list of skus of the kit of wepaon, amror, and item with the respective "closest" vectors as our recommendation
 
-    return rec_item_sku[0]
+    return [rec_weapon_sku[0],rec_armor_sku[0], rec_misc_sku[0]]
 
+
+
+# , curr_weapon:str, curr_armor:str, curr_misc:str
