793de40e7dcf66eda5908f3a46ed21ec1402c3ed
James Irwin <jamesirwin0211@gmail.com>
2024-05-30 18:59:38 -0700

    add item_vectors to schema.sql

--- schema.sql
+++ schema.sql
@@ -69,35 +69,35 @@ INSERT INTO mods_plan VALUES (3, 'WATER', 0, ARRAY['weapon', 'armor', 'other'], 
 
 -- insert all possible items into plan
 
-INSERT INTO items_plan VALUES (1, 'weapon', 30, 0, 'LONGSWORD');
-INSERT INTO items_plan VALUES (2, 'weapon', 50, 1, 'FIRE_LONGSWORD');
-INSERT INTO items_plan VALUES (3, 'weapon', 55, 2, 'EARTH_LONGSWORD');
-INSERT INTO items_plan VALUES (4, 'weapon', 45, 3, 'WATER_LONGSWORD');
+INSERT INTO items_plan VALUES (1, 'weapon', 30, 0, 'LONGSWORD', ARRAY[30, 0, 1]);
+INSERT INTO items_plan VALUES (2, 'weapon', 50, 1, 'FIRE_LONGSWORD', ARRAY[50, 1, 1]);
+INSERT INTO items_plan VALUES (3, 'weapon', 55, 2, 'EARTH_LONGSWORD', ARRAY[55, 2, 1]);
+INSERT INTO items_plan VALUES (4, 'weapon', 45, 3, 'WATER_LONGSWORD', ARRAY[45, 3, 1]);
 
-INSERT INTO items_plan VALUES (5, 'weapon', 25, 0, 'PISTOL');
-INSERT INTO items_plan VALUES (6, 'weapon', 45, 1, 'FIRE_PISTOL');
-INSERT INTO items_plan VALUES (7, 'weapon', 50, 2, 'EARTH_PISTOL');
-INSERT INTO items_plan VALUES (8, 'weapon', 40, 3, 'WATER_PISTOL');
+INSERT INTO items_plan VALUES (5, 'weapon', 25, 0, 'PISTOL', ARRAY[25, 0, 1]);
+INSERT INTO items_plan VALUES (6, 'weapon', 45, 1, 'FIRE_PISTOL', ARRAY[45, 1, 1]);
+INSERT INTO items_plan VALUES (7, 'weapon', 50, 2, 'EARTH_PISTOL', ARRAY[50, 2, 1]);
+INSERT INTO items_plan VALUES (8, 'weapon', 40, 3, 'WATER_PISTOL', ARRAY[40, 3, 1]);
 
-INSERT INTO items_plan VALUES (9, 'armor', 40, 0, 'SHIELD');
-INSERT INTO items_plan VALUES (10, 'armor', 60, 1, 'FIRE_SHIELD');
-INSERT INTO items_plan VALUES (11, 'armor', 65, 2, 'EARTH_SHIELD');
-INSERT INTO items_plan VALUES (12, 'armor', 55, 3, 'WATER_SHIELD');
+INSERT INTO items_plan VALUES (9, 'armor', 40, 0, 'SHIELD', ARRAY[40, 0, 2]);
+INSERT INTO items_plan VALUES (10, 'armor', 60, 1, 'FIRE_SHIELD', ARRAY[60, 1, 2]);
+INSERT INTO items_plan VALUES (11, 'armor', 65, 2, 'EARTH_SHIELD', ARRAY[65, 2, 2]);
+INSERT INTO items_plan VALUES (12, 'armor', 55, 3, 'WATER_SHIELD', ARRAY[55, 3, 2]);
 
-INSERT INTO items_plan VALUES (13, 'armor', 25, 0, 'HELMET');
-INSERT INTO items_plan VALUES (14, 'armor', 45, 1, 'FIRE_HELMET');
-INSERT INTO items_plan VALUES (15, 'armor', 50, 2, 'EARTH_HELMET');
-INSERT INTO items_plan VALUES (16, 'armor', 40, 3, 'WATER_HELMET');
+INSERT INTO items_plan VALUES (13, 'armor', 25, 0, 'HELMET', ARRAY[25, 0, 2]);
+INSERT INTO items_plan VALUES (14, 'armor', 45, 1, 'FIRE_HELMET', ARRAY[45, 1, 2]);
+INSERT INTO items_plan VALUES (15, 'armor', 50, 2, 'EARTH_HELMET', ARRAY[50, 2, 2]);
+INSERT INTO items_plan VALUES (16, 'armor', 40, 3, 'WATER_HELMET', ARRAY[40, 3, 2]);
 
-INSERT INTO items_plan VALUES (17, 'other', 35, 0, 'STAFF');
-INSERT INTO items_plan VALUES (18, 'other', 55, 1, 'FIRE_STAFF');
-INSERT INTO items_plan VALUES (19, 'other', 60, 2, 'EARTH_STAFF');
-INSERT INTO items_plan VALUES (20, 'other', 50, 3, 'WATER_STAFF');
+INSERT INTO items_plan VALUES (17, 'other', 35, 0, 'STAFF', ARRAY[35, 0, 3]);
+INSERT INTO items_plan VALUES (18, 'other', 55, 1, 'FIRE_STAFF', ARRAY[55, 1, 3]);
+INSERT INTO items_plan VALUES (19, 'other', 60, 2, 'EARTH_STAFF', ARRAY[60, 2, 3]);
+INSERT INTO items_plan VALUES (20, 'other', 50, 3, 'WATER_STAFF', ARRAY[50, 3, 3]);
 
-INSERT INTO items_plan VALUES (21, 'other', 30, 0, 'CHAINLINK');
-INSERT INTO items_plan VALUES (22, 'other', 50, 1, 'FIRE_CHAINLINK');
-INSERT INTO items_plan VALUES (23, 'other', 55, 2, 'EARTH_CHAINLINK');
-INSERT INTO items_plan VALUES (24, 'other', 45, 3, 'WATER_CHAINLINK');
+INSERT INTO items_plan VALUES (21, 'other', 30, 0, 'CHAINLINK', ARRAY[30, 0, 3]);
+INSERT INTO items_plan VALUES (22, 'other', 50, 1, 'FIRE_CHAINLINK', ARRAY[50, 1, 3]);
+INSERT INTO items_plan VALUES (23, 'other', 55, 2, 'EARTH_CHAINLINK', ARRAY[55, 2, 3]);
+INSERT INTO items_plan VALUES (24, 'other', 45, 3, 'WATER_CHAINLINK', ARRAY[45, 3, 3]);
 
 -- NOTE: this is causing the first mod to not work for some reason .. 
 INSERT INTO mods_ledger VALUES (2, now(), 0, 0, 'NONE', 2000);
