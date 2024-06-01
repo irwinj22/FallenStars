f0fe5ba9aa7213e1718923ce2637432b122e10e5
James Irwin <jamesirwin0211@gmail.com>
2024-06-01 14:46:22 -0700

    schema changes

--- src/api/mods.py
+++ src/api/mods.py
@@ -22,40 +22,56 @@ def attach_mods():
     Attaching mods to items. 
     '''
      
+    # TODO: this function does not work properly ... 
+    # attaching even after it runs out of items to attach to 
+    
     with db.engine.begin() as connection:
         base_items = connection.execute(sqlalchemy.text("""SELECT item_id, item_sku, type, SUM(qty_change) AS qty_change FROM items_ledger
                                            JOIN items_plan ON items_ledger.item_id = items_plan.id
                                            WHERE items_plan.mod_id = 0 and items_ledger.customer_id = 0
                                            GROUP BY item_id, item_sku, type
                                            HAVING SUM(qty_change) > 0"""))
+        
         planned_items = connection.execute(sqlalchemy.text("SELECT * FROM items_plan WHERE NOT mod_id = 0"))
+
         mod_catalog = connection.execute(sqlalchemy.text("""SELECT mods_plan.id, mods_plan.sku, mods_plan.type, sum(qty_change) AS quantity 
                                                          FROM mods_ledger JOIN mods_plan ON mods_ledger.mod_id = mods_plan.id 
                                                          GROUP BY mods_plan.id, mods_plan.sku, mods_plan.type"""))
-
-        base_items_dict = [row._asdict() for row in base_items.fetchall()] # Rows saved as dictionaries of all items eligible to be modded
-        planned_items_dict = [row._asdict() for row in planned_items.fetchall()] # All possible planned items saved as dictionaries
+       
+        # Rows saved as dictionaries of all items eligible to be modded
+        base_items_dict = [row._asdict() for row in base_items.fetchall()] 
+        # All possible planned items saved as dictionaries
+        planned_items_dict = [row._asdict() for row in planned_items.fetchall()] 
 
         final_items = []
+        # TODO: these aren't dictionaries though? 
         used_items_dict = []
-        mod_catalog_dict =[]
+        mod_catalog_dict = []
+
         for mod in mod_catalog:
             quantity = mod.quantity
             for item in base_items_dict:
-                if quantity != 0: # If there are mods that are available and we have weapons to mod, add them!
+                # If there are mods that are available and we have weapons to mod, add them!
+                if quantity != 0: 
                     used_item = item
-                    used_item["qty_change"] = -1*min(abs(item["qty_change"]), abs(mod.quantity)) # Attach until we run out of weapons or mods.
+                    # Attach until we run out of weapons or mods.
+                    used_item["qty_change"] = -1*min(abs(item["qty_change"]), abs(mod.quantity)) 
                     used_item["credit_change"] = 0 
+                    # Dictionary row to add to the ledger
                     final_items += [{"qty_change": -1*used_item["qty_change"], 
                                      "item_id": next(i["id"] for i in planned_items_dict if i["sku"] == mod.sku + "_" + item["item_sku"]),
                                      "item_sku": mod.sku + "_" + item["item_sku"],
                                      "credit_change": 0,
-                                     "customer_change": 0}] # Dictionary row to add to the ledger
-                    used_items_dict += [used_item] # Dictionary row to add to the ledger, subtracts base item
-                    quantity += used_item["qty_change"] # The quantity variable keeps track of how many mods are left to use
+                                     "customer_change": 0}]
+                    # Dictionary row to add to the ledger, subtracts base item
+                    used_items_dict += [used_item] 
+                    # The quantity variable keeps track of how many mods are left to use
+                    quantity += used_item["qty_change"] 
+
             # only want to add to mod ledger if there is quantity change
             if quantity - mod.quantity != 0:
                 mod_catalog_dict += [{"qty_change": quantity - mod.quantity, "mod_id": mod.id, "mod_sku": mod.sku, "credit_change": 0}]
+        
         if used_items_dict and final_items:
             connection.execute(sqlalchemy.text("""INSERT INTO items_ledger (qty_change, item_id, item_sku, credit_change) 
                                                VALUES (:qty_change, :item_id, :item_sku, :credit_change)"""), used_items_dict + final_items) # Insert all changes (Base items removed), modded versions added
